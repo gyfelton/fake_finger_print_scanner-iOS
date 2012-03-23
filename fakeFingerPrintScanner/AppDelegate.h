ada08ed36f943ae7d0579fa6c3acdb6788665647
Yuanfeng Gao <gyfelton@gmail.com>
2012-03-23 15:32:30 +0800

    Initial Commit

---
+++ fakeFingerPrintScanner/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  fakeFingerPrintScanner
+//
+//  Created by Yuanfeng on 12-03-23.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ fakeFingerPrintScanner/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  fakeFingerPrintScanner
+//
+//  Created by Yuanfeng on 12-03-23.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
